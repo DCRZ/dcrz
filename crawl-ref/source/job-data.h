b-crawl/bcrawl

Make Warper a ranged background

crawl-ref/source/job-data.h
@@ -272,14 +272,12 @@ static const map<job_type, job_def> job_data =
 { JOB_WARPER, {
     "Wr", "Warper",
     3, 5, 4,
-    { SP_FELID, SP_KOBOLD, SP_DEEP_DWARF, SP_SPRIGGAN, SP_CENTAUR,
-      SP_BASE_DRACONIAN, },
-    { "leather armour", "book of Spatial Translocations", "scroll of blinking",
-      "tomahawk ego:dispersal q:5" },
-    WCHOICE_PLAIN,
-    { { SK_FIGHTING, 2 }, { SK_ARMOUR, 1 }, { SK_DODGING, 2 },
-      { SK_SPELLCASTING, 2 }, { SK_TRANSLOCATIONS, 3 }, { SK_THROWING, 1 },
-      { SK_WEAPON, 2 }, },
+    { SP_KOBOLD, SP_DEEP_DWARF, SP_SPRIGGAN, SP_CENTAUR,
+      SP_BARACHI, SP_HUMAN },
+    { "robe", "book of Spatial Translocations" },
+    WCHOICE_RANGED,
+    { { SK_FIGHTING, 1 }, { SK_DODGING, 2 }, { SK_SPELLCASTING, 1 },
+      { SK_TRANSLOCATIONS, 3 }, { SK_WEAPON, 2 }, },
 } },
 
 { JOB_WIZARD, {

DCRZ/dcrz

crawl-ref/source/job-data.h
@@ -262,14 +262,12 @@ static const map<job_type, job_def> job_data =
 { JOB_WARPER, {
     "Wr", "Warper",
     3, 5, 4,
-    { SP_FELID, SP_HALFLING, SP_DEEP_DWARF, SP_SPRIGGAN, SP_CENTAUR,
-      SP_BASE_DRACONIAN, },
-    { "leather armour", "book of Spatial Translocations", "scroll of blinking",
-      "boomerang ego:dispersal q:5" },
-    WCHOICE_PLAIN,
-    { { SK_FIGHTING, 2 }, { SK_ARMOUR, 1 }, { SK_DODGING, 2 },
-      { SK_SPELLCASTING, 2 }, { SK_TRANSLOCATIONS, 3 }, { SK_THROWING, 1 },
-      { SK_WEAPON, 2 }, },
+    { SP_HALFLING, SP_DEEP_DWARF, SP_SPRIGGAN, SP_CENTAUR,
+      SP_BARACHI, SP_HUMAN, },
+    { "robe", "book of Spatial Translocations" },
+    WCHOICE_RANGED,
+    { { SK_FIGHTING, 1 }, { SK_DODGING, 2 }, { SK_SPELLCASTING, 1 },
+      { SK_TRANSLOCATIONS, 3 }, { SK_WEAPON, 2 }, },
 } },
 
 { JOB_WIZARD, {