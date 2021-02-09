883d672b7b62975034b3d44227a79a2c1b050c9b
AdamPG <adampgardner@gmail.com>
2021-02-09 19:50:24 -0330

    Adjust the depth of some more vaults
    Many vaults were overlooked when shortening the Dungeon.

--- crawl-ref/source/dat/dlua/explorer.lua
+++ crawl-ref/source/dat/dlua/explorer.lua
@@ -9,7 +9,7 @@ util.namespace('explorer')
 explorer.generation_order = {
                 "D:1",
                 "D:2", "D:3", "D:4", "D:5", "D:6", "D:7", "D:8", "D:9",
-                "D:10", "D:11", "D:12", "D:13", "D:14", "D:15",
+                "D:10", "D:11", "D:12",
                 "Temple",
                 "Lair:1", "Lair:2", "Lair:3", "Lair:4"
                 "Orc:1",
