798252972c76267406dd77e261379d84ba977952
AdamPG <adampgardner@gmail.com>
2021-03-06 18:18:44 -0330

    Add potions of cure mutation back to vaults and Ziggurats

--- crawl-ref/source/dat/dlua/ziggurat.lua
+++ crawl-ref/source/dat/dlua/ziggurat.lua
@@ -585,7 +585,8 @@ local function ziggurat_create_loot_at(c)
                                   dgn.good_scrolls)
   local super_loot = dgn.item_spec("| no_pickup w:7000 /" ..
                                    "potion of experience no_pickup w:190 q:1 /" ..
-                                   "potion of mutation no_pickup w:220 /" ..
+                                   "potion of cure mutation no_pickup w:190 /" ..
+                                   "potion of mutation no_pickup w:80 /" ..
                                    "ration no_pickup w:80 /" ..
                                    "potion of cancellation q:5 no_pickup / " ..
                                    "potion of heal wounds q:5 no_pickup / " ..
